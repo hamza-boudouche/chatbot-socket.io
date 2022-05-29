332e5cf0224b8510935041b6e79256deaedd1549
AmalDer <amalderouich00@gmail.com>
2022-05-29 17:32:59 +0100

    modify an event

--- app.js
+++ app.js
@@ -49,20 +49,6 @@ app.get('/api/external', function (req, res) {
 	res.json(`You are ${req.user}`);
 });
 
-// app.post('/api/v1/calendar', function (req, res) {
-// 	res.json({
-// 		"summary": "Testing",
-// 		"location": "Rabat",
-// 		"description": "Testing the post method",
-// 		"start": "2022-04-22T18:14:00+00:00",
-// 		"end": "2022-04-22T20:15:00+00:00",
-// 		"participant": [
-// 			"oussamabouzekraoui01@gmail.com",
-// 			"oussamabouzekraoui01@gmail.com"
-// 		]
-// 	})
-// })
-
 app.put('/api/v1/calendar/:id', function (req, res) {
 	res.json(
 		{
@@ -75,85 +61,6 @@ app.put('/api/v1/calendar/:id', function (req, res) {
 		}
   )
 })
-
-// app.delete('/api/v1/calendar/something/:id', function (req, res) {
-// 	res.json({
-// 		"summary":"Testing",
-// 		"location":"Rabat",
-// 		"description":"Testing the delete method",
-// 		"participant":[
-// 			"oussamabouzekraoui01@gmail.com",
-// 			"oussamabouzekraoui@student.emi.ac.ma"
-// 		]
-// 	  })
-// })
-
-// app.get('/api/v1/calendar/:start/:end', (req, res) => {
-// 	res.json([
-// 		{
-// 			"attendees": [
-// 				{
-// 					"email": "oussamabouzekraoui01@gmail.com",
-// 					"organizer": true,
-// 					"responseStatus": "needsAction",
-// 					"self": true
-// 				},
-// 				{
-// 					"email": "oussamabouzekraoui@student.emi.ac.ma",
-// 					"responseStatus": "needsAction"
-// 				}
-// 			],
-// 			"created": {
-// 				"value": 1648408504000,
-// 				"dateOnly": false,
-// 				"timeZoneShift": 0
-// 			},
-// 			"creator": {
-// 				"email": "oussamabouzekraoui01@gmail.com",
-// 				"self": true
-// 			},
-// 			"description": "Testing the put method",
-// 			"end": {
-// 				"dateTime": {
-// 					"value": 1648335600000,
-// 					"dateOnly": false,
-// 					"timeZoneShift": 60
-// 				},
-// 				"timeZone": "Africa/Casablanca"
-// 			},
-// 			"etag": "\"3296817316392000\"",
-// 			"htmlLink": "https://www.google.com/calendar/event?eid=YWZmbDlqcmpyMWZsOHFxMmhvdWxndG5udjggb3Vzc2FtYWJvdXpla3Jhb3VpMDFAbQ",
-// 			"iCalUID": "affl9jrjr1fl8qq2houlgtnnv8@google.com",
-// 			"id": "affl9jrjr1fl8qq2houlgtnnv8",
-// 			"kind": "calendar#event",
-// 			"location": "Rabat",
-// 			"organizer": {
-// 				"email": "oussamabouzekraoui01@gmail.com",
-// 				"self": true
-// 			},
-// 			"reminders": {
-// 				"useDefault": true
-// 			},
-// 			"sequence": 0,
-// 			"start": {
-// 				"dateTime": {
-// 					"value": 1648332000000,
-// 					"dateOnly": false,
-// 					"timeZoneShift": 60
-// 				},
-// 				"timeZone": "Africa/Casablanca"
-// 			},
-// 			"status": "confirmed",
-// 			"summary": "Testing",
-// 			"updated": {
-// 				"value": 1648408658196,
-// 				"dateOnly": false,
-// 				"timeZoneShift": 0
-// 			},
-// 			"eventType": "default"
-// 		}
-// 	])
-// })
 
 const handleAddEventRequest = async (info) => {
 	const startTime = await formatDate(info.startTime);
@@ -217,23 +124,6 @@ io.on('connection', async (socket) => {
 			socket.emit("error", error)
 		}
 	})
-	// socket.on('delete_event', async (message) => {
-	// 	const info = await handleAddEventRequest(message)
-	// 	try {
-	// 		const host = "http://localhost:5034"
-	// 		const finalReq = await axios.delete(`${host}/api/v1/calendar/something/${info.id}`, {
-	// 			summary: info.title,
-	// 			location: "Rabat",
-	// 			description: info.description,
-	// 			start: info.startTime,
-	// 			end: info.endTime,
-	// 			participant: ["test@test.com"]
-	// 		})
-	// 		socket.emit("reply", [{ text: "event deleted successfully" }])
-	// 	} catch (error) {
-	// 		socket.emit("error", error)
-	// 	}
-	// })
 	socket.on('disconnect', () => {
 		console.log("user disconnected")
 	})
